92f9154a3fc0eb9aef63158df2c245a27237d8ae
Gun-Hee-SON <songunhee5426@gmail.com>
2024-07-19 17:14:24 +0900

    손건희 포트폴리오 홈페이지 시작

---
+++ sonpage/src/main/java/com/mysite/sonpage/SonpageApplication.java
@@ -0,0 +1,13 @@
+package com.mysite.sonpage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SonpageApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SonpageApplication.class, args);
+	}
+
+}
---
+++ sonpage/src/test/java/com/mysite/sonpage/SonpageApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mysite.sonpage;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SonpageApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
